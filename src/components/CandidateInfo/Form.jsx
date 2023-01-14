a28c0b060748285cf94081a05ea3ad971108e855
Pei Yong <peiyong0890@gmail.com>
2023-01-14 18:43:50 +0800

    Changes in input

--- src/components/CandidateInfo/Form.jsx
+++ src/components/CandidateInfo/Form.jsx
@@ -17,16 +17,16 @@ function Form() {
 			</div>
 			<div className="grid grid-cols-6 gap-4 col-span-full lg:col-span-3 justify-center">
 				<div className="col-span-full sm:col-span-3">
-					<label for="first-name" className="font-projectFontfont-medium text-sm">First Name</label>
-					<input id="first-name" type="text" placeholder="First name" className="w-full font-projectFont rounded-md focus:ring focus:ring-opacity-75 focus:ring-blue-400 " />
+					<label for="first-name" className="font-projectFont font-medium text-sm">First Name</label>
+					<input id="first-name" type="text" placeholder="First name" className="w-full font-projectFont rounded-md border-blue" />
 				</div>
 				<div className="col-span-full sm:col-span-3">
 					<label for="last-name" className="font-projectFont font-medium text-sm">Last Name</label>
-					<input id="last-name" type="text" placeholder="Last name" className="w-full font-projectFont rounded-md focus:ring focus:ring-opacity-75 focus:ring-blue-400" />
+					<input id="last-name" type="text" placeholder="Last name" className="w-full font-projectFont rounded-md border-blue" />
 				</div>
 				<div className="col-span-full sm:col-span-3">
 					<label for="qualification" className="font-projectFont font-medium text-sm">Gender</label>
-                    <select className="w-full p-2 font-projectFont text-[#64748b] text-base bg-white border rounded-md shadow-sm  focus:ring focus:ring-opacity-75 focus:border-blue-400">
+                    <select className="w-full p-2 font-projectFont text-[#64748b] text-base bg-white border rounded-md shadow-sm border-blue">
                         <option disabled selected>Gender</option>
                         <option>Male</option>
                         <option>Female</option>
@@ -34,23 +34,23 @@ function Form() {
 				</div>
 				<div className="col-span-full sm:col-span-3">
 					<label for="contact" className="font-projectFont font-medium text-sm">Contact Number</label>
-					<input id="contact" type="text" placeholder="with country code (60123456789)" className="w-full font-projectFont rounded-md focus:ring focus:ring-opacity-75 focus:ring-blue-400" />
+					<input id="contact" type="text" placeholder="with country code (60123456789)" className="w-full font-projectFont rounded-md border-blue" />
 				</div>
 				<div className="col-span-full">
 					<label for="email" className="font-projectFont font-medium text-sm">Email</label>
-					<input id="email" type="email" placeholder="Email" className="w-full font-projectFont rounded-md focus:ring focus:ring-opacity-75 focus:ring-blue-400" />
+					<input id="email" type="email" placeholder="Email" className="w-full font-projectFont rounded-md border-blue" />
 				</div>
 				<div className="col-span-full sm:col-span-2">
 					<label for="country" className="font-projectFont font-medium text-sm">Country</label>
-					<input id="country" type="text" placeholder="" className="w-full font-projectFont rounded-md focus:ring focus:ring-opacity-75 focus:ring-blue-400" />
+					<input id="country" type="text" placeholder="" className="w-full font-projectFont rounded-md border-blue" />
 				</div>
 				<div className="col-span-full sm:col-span-2">
 					<label for="state" className="font-projectFont font-medium text-sm">State / Province</label>
-					<input id="state" type="text" placeholder="" className="w-full font-projectFont rounded-md focus:ring focus:ring-opacity-75 focus:ring-blue-400" />
+					<input id="state" type="text" placeholder="" className="w-full font-projectFont rounded-md border-blue" />
 				</div>
 				<div className="col-span-full sm:col-span-2">
 					<label for="city" className="font-projectFont font-medium text-sm">City</label>
-					<input id="city" type="text" placeholder="" className="w-full font-projectFont rounded-md focus:ring focus:ring-opacity-75 focus:ring-blue-400" />
+					<input id="city" type="text" placeholder="" className="w-full font-projectFont rounded-md border-blue" />
 				</div>
 			</div>
 		    </fieldset>
@@ -61,11 +61,11 @@ function Form() {
 			<div className="grid grid-cols-6 gap-4 col-span-full lg:col-span-3">
                 <div className="col-span-full">
 					<label for="university" className="font-projectFont font-medium text-sm">University / Institute</label>
-					<input id="university" type="text" placeholder="University / Institute name" className="w-full font-projectFont rounded-md focus:ring focus:ring-opacity-75  focus:ring-blue-400" />
+					<input id="university" type="text" placeholder="University / Institute name" className="w-full font-projectFont rounded-md border-blue" />
 				</div>
 				<div className="col-span-full sm:col-span-3">
 					<label for="qualification" className="font-projectFont font-medium text-sm">Qualification</label>
-                    <select className="w-full p-2 font-projectFont text-[#64748b] text-base bg-white border rounded-md shadow-sm  focus:ring focus:ring-opacity-75 focus:border-blue-400">
+                    <select className="w-full p-2 font-projectFont text-[#64748b] text-base bg-white border rounded-md shadow-sm border-blue">
                         <option disabled selected>Qualification</option>
                         <option>Primary/Seconday school/SPM/"O" level</option>
                         <option>Higher Secondary/STPM/"A" level/Pre-U</option>
@@ -80,7 +80,7 @@ function Form() {
 				</div>
                 <div className="col-span-full sm:col-span-3">
 					<label for="studyField" className="font-projectFont font-medium text-sm">Field of Study</label>
-					<input id="studyField" type="text" placeholder="" className="w-full font-projectFont rounded-md focus:ring focus:ring-opacity-75 focus:ring-blue-400" />
+					<input id="studyField" type="text" placeholder="" className="w-full font-projectFont rounded-md border-blue" />
 				</div>
 			</div>
 		    </fieldset>
@@ -88,6 +88,7 @@ function Form() {
         <div className='flex flex-row-reverse mt-8 m-auto text-center'>
             <button className='px-8 py-3 mb-8 rounded-lg border font-projectFont text-base font-medium bg-blue text-white hover:bg-blue-500 hover:scale-105 active:bg-blue-200'>Save</button>
         </div>
+
 </section>
 
   )
--- src/components/EmployerInfo/Form.jsx
+++ src/components/EmployerInfo/Form.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { HiBuildingOffice } from 'react-icons/hi2';
 
-
 function Form() {
-  return (
+	
+	return (
     <section className="p-6">
         <h2 className='text-xl md:text-2xl lg:3xl font-projectFont font-bold text-center text-orange mt-24 mb-8'>Tell us about your company</h2>
         <form novalidate="" action="" className="container flex flex-col mx-auto font-projectFont space-y-12 ng-untouched ng-pristine ng-valid">
@@ -15,42 +15,64 @@ function Form() {
   					</div>
 				</div> 
 			</div>
-			<div className="grid grid-cols-6 gap-4 col-span-full lg:col-span-3 justify-center">
+			<div className="grid grid-cols-6 gap-4 col-span-full lg:col-span-3 justify-center text-black">
 				<div className="col-span-full sm:col-span-3">
-					<label for="company-name" className="font-projectFont font-medium text-sm">Company Name</label>
-					<input id="company-name" type="text" placeholder="Example Sdn. Bhd." className="w-full font-projectFont rounded-md focus:ring focus:ring-opacity-75 focus:ring-orange-400 " />
+					<label for="company-name" className="font-projectFont font-mediu text-sm">Company Name</label>
+					<input id="company-name" type="text" placeholder="Example Sdn. Bhd." className="w-full font-projectFont rounded-md border-orange active:bg-white active:text-black" />
 				</div>
 				<div className="col-span-full sm:col-span-3">
 					<label for="company-industry" className="font-projectFont font-medium text-sm">Industry</label>
-					<input id="company-industry" type="text" placeholder="Industry" className="w-full font-projectFont rounded-md focus:ring focus:ring-opacity-75 focus:ring-orange-400" />
+                    <select id="company-instry" className="w-full p-2 font-projectFont text-[#64748b] text-base bg-white border rounded-md shadow-sm border-orange ">
+                        <option disabled selected>Industry</option>
+                        <option>Electronics Manufacturing</option>
+                        <option>Semiconductors</option>
+                        <option>Microchips</option>
+                        <option>Integrated Circuits</option>
+                        <option>Telecommunications Services</option>
+                        <option>Computer Hardware Development</option>
+                        <option>Enterprise Software & Network Solutions</option>
+                        <option>Internet & Web Services</option>
+                        <option>Information Technology Support Sevices</option>
+						<option>Biotech & Pharmaceuticals</option>
+						<option>Cybersecurity Consulting</option>
+					</select>
 				</div>
-                <div className="col-span-full sm:col-span-3">
-					<label for="company-size" className="font-projectFont font-medium text-sm">Company Size</label>
-					<input id="company-size" type="text" placeholder="Company size" className="w-full font-projectFont rounded-md focus:ring focus:ring-opacity-75 focus:ring-orange-400" />
+				<div className="col-span-full sm:col-span-3">
+					<label for="company-industry" className="font-projectFont font-medium text-sm">Company Size</label>
+                    <select id="company-size" className="w-full p-2 font-projectFont text-[#64748b] text-base bg-white border rounded-md shadow-sm border-orange">
+                        <option disabled selected>Company Size</option>
+                        <option>1 - 50 employees</option>
+                        <option>51 - 100 employees</option>
+                        <option>101 - 500 employees</option>
+                        <option>501 - 1000 employees</option>
+                        <option>1001 - 5000 employees</option>
+                        <option>above 5000 employees</option>
+                        <option>above 10000 employees</option>
+					</select>
 				</div>
 				<div className="col-span-full sm:col-span-3">
 					<label for="founded" className="font-projectFont font-medium text-sm">Founded</label>
-					<input id="founded" type="text" placeholder="Year" className="w-full font-projectFont rounded-md focus:ring focus:ring-opacity-75 focus:ring-orange-400" />
+					<input id="founded" type="date" className="w-full font-projectFont  text-[#64748b] rounded-md border-orange"/>	
 				</div>
 				<div className="col-span-full sm:col-span-3">
 					<label htmlFor="company-website" className="font-projectFont font-medium text-sm">Company Website</label>
-					<input id="company-website" type="text" placeholder="www.company.com" className="w-full font-projectFont rounded-md focus:ring focus:ring-opacity-75 focus:ring-orange-400" />
+					<input id="company-website" type="text" placeholder="www.company.com" className="w-full font-projectFont rounded-md  border-orange active:bg-white active:text-black" />
 				</div>
 				<div className="col-span-full sm:col-span-3">
 					<label for="company-email" className="font-projectFont font-medium text-sm">Company Email</label>
-					<input id="company-email" type="email" placeholder="email@company.com" className="w-full font-projectFont rounded-md focus:ring focus:ring-opacity-75 focus:ring-orange-400" />
+					<input id="company-email" type="email" placeholder="email@company.com" className="w-full font-projectFont rounded-md  border-orange" />
 				</div> 
 				<div className="col-span-full sm:col-span-2">
 					<label for="company-country" className="font-projectFont font-medium text-sm">Country</label>
-					<input id="company-country" type="text" placeholder="" className="w-full font-projectFont rounded-md focus:ring focus:ring-opacity-75 focus:ring-orange-400" />
+					<input id="company-country" type="text" placeholder="" className="w-full font-projectFont rounded-md border-orange" />
 				</div>
 				<div className="col-span-full sm:col-span-2">
 					<label for="company-state" className="font-projectFont font-medium text-sm">State / Province</label>
-					<input id="company-state" type="text" placeholder="" className="w-full font-projectFont rounded-md focus:ring focus:ring-opacity-75 focus:ring-orange-400" />
+					<input id="company-state" type="text" placeholder="" className="w-full font-projectFont rounded-md border-orange" />
 				</div>
 				<div className="col-span-full sm:col-span-2">
 					<label for="company-city" className="font-projectFont font-medium text-sm">City</label>
-					<input id="company-city" type="text" placeholder="" className="w-full font-projectFont rounded-md focus:ring focus:ring-opacity-75 focus:ring-orange-400" />
+					<input id="company-city" type="text" placeholder="" className="w-full font-projectFont rounded-md border-orange" />
 				</div>
 			</div>
 		    </fieldset>
@@ -58,6 +80,7 @@ function Form() {
         <div className='flex flex-row-reverse mt-8 m-auto text-center'>
             <button className='px-8 py-3 mb-8 rounded-lg border font-projectFont text-base font-medium bg-orange text-white hover:bg-orange-500 hover:scale-105 active:bg-orange-200'>Save</button>
         </div>
+		
 </section>
 
   )
