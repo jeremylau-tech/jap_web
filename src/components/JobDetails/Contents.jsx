6594e8996168e77d81e9bfcaf4b91785c3a5f774
yenting2001 <yenting2001@yahoo.com.my>
2023-01-15 19:53:23 +0800

    Update JobDetails Content

--- src/components/JobDetails/Contents.jsx
+++ src/components/JobDetails/Contents.jsx
@@ -3,17 +3,17 @@ import React from 'react'
 function Content() {
   return (
   <div className='w-full py-[80px] bg-white px-10 pt-6 pb-8'>
-    <figure className='px-8 pt-6 pb-8 mb-4'><img src="https://www.sparrowsph.my/image/data/theme/Entrepreneurs%20Listed/vitrox-chu-logo_030922231458.jpg" alt="Company" img-height='1px'/></figure> 
+    <figure className='h-[150px] w-[350px] mx-auto inline-flex px-10'><img src="https://www.sparrowsph.my/image/data/theme/Entrepreneurs%20Listed/vitrox-chu-logo_030922231458.jpg" alt="Company" img-height='1px'/></figure> 
     <div className='shadow-md rounded px-8 pt-6 pb-8 mb-4'>
-        <h1 className='text-[#F68B1F] font-projectFont md:text-3xl sm:text-2xl text-2xl font-bold text-left pb-4'>Job Title</h1>
-        <h3 className='text-[#311B92] font-projectFont md:text-xl sm:text-l text-m font-medium text-left pb-4'>Company Name</h3>
+        <h1 className='text-[#F68B1F] font-projectFont md:text-3xl sm:text-3xl text-3xl font-bold text-left pb-4'>Job Title</h1>
+        <h3 className='text-[#311B92] font-projectFont md:text-xl sm:text-xl text-m font-medium text-left pb-4'>Company Name</h3>
         <h4 className='text-[#311B92] font-projectFont md:text-l sm:text-m text-s font-regular text-left pb-4'>Location</h4>
     </div>
     <div className='max-w-[1240px] mx-8 md:mx-auto grid md:grid-cols-3 gap-2 md:gap-8;'>
     </div>
 
     <div className='shadow-md rounded px-10 pt-6 pb-8 mb-4'>
-    <h3 className='font-projectFont text-[#311B92] md:text-xl sm:text-l text-m font-bold text-left pb-4'>Job Details</h3>
+    <h3 className='font-projectFont text-[#311B92] md:text-2xl sm:text-2xl text-xl font-bold text-left pb-4'>Job Details</h3>
         <p className='text-[#000000] md:text-l sm:text-m text-s font-regular text-left pb-4'>
             <p className='font-projectFont font-bold'>Requirements:</p>
         <ul className='font-projectFont list-style-type-disc'>
